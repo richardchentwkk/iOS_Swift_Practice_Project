ac4e70164524f9f58016dba6ebb80fe36e80d0a4
richard <richardchentwkk@gmail.com>
2019-02-14 18:22:12 +0800

    [iOS] Init Magic pills

---
+++ iOS-practice-MagicPills/magicPill/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  magicPill
+//
+//  Created by Richard on 2019/2/14.
+//  Copyright © 2019 Richard. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
